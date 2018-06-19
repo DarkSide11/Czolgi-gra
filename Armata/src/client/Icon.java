0f75e79663e39c41e397fef2a15f38667902fdd2
Robert <Robert@Bobby.local>
2018-06-19 15:49:34 +0200

    Odsmiecanie klasy Icon

--- Armata/src/client/Icon.java
+++ Armata/src/client/Icon.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class Icon  {
-<<<<<<< HEAD
+
 	private Image image;
 	private ImageView imageView;
 	private double x;
@@ -14,16 +14,7 @@ public class Icon  {
 	private boolean active=false;
 	private double height;
 	private double width;
-=======
-	Image image;
-	ImageView imageView;
-	double x;
-	double y; 
-	Pane pane;
-	boolean active=false;
-	double height;
-	double width;
->>>>>>> refs/remotes/origin/master1
+
 	
 	
 	public Icon(Pane pane,double xPos,double yPos, String ImageURL) {
@@ -40,19 +31,12 @@ public class Icon  {
 }
 	
 	public void render(double x, double y) {		
-<<<<<<< HEAD
-		imageView.relocate(x, y); 
-=======
 		imageView.relocate(x, y);	
->>>>>>> refs/remotes/origin/master1
 	}
 	
 	public void hide( ) {
 		imageView.setVisible(false);
 	}
-	
-	
-	
-	
+			
 }
 
